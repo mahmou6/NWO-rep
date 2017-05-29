b59f0b3e6e9920b5d12d09444d56e66136db0093
mahmou6 <mahmou6@gmail.com>
2017-05-29 12:49:56 +0200

    The Bye for now commit.

--- NWO/ViewController.swift
+++ NWO/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hiya!")
+        print("are you done yet")
+        Print("Bye for now!")
     }
 
     override func didReceiveMemoryWarning() {
