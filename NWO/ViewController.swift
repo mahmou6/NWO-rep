6b20ff2dc487612d0b54e609ca5119078c6beccb
mahmou6 <mahmou6@gmail.com>
2017-05-30 11:57:32 +0200

    Revert "Typo at the print bye from now line"
    This reverts commit f2b79410a35493745eb7b78a85c7ed2b0c54eb4d.

--- NWO/ViewController.swift
+++ NWO/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Hiya!")
         print("are you done yet")
-        print("Bye for now!")
+        Print("Bye for now!")
     }
 
     override func didReceiveMemoryWarning() {
