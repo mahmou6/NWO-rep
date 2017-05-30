48b6814d11246bb466717385278d96492d77631d
mahmou6 <mahmou6@gmail.com>
2017-05-30 12:01:03 +0200

    Typo fixed
    Print bye for now typo fixed. The typo was in Print instead of print.

--- NWO/ViewController.swift
+++ NWO/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Hiya!")
         print("are you done yet")
-        Print("Bye for now!")
+        print("Bye for now!")
     }
 
     override func didReceiveMemoryWarning() {
